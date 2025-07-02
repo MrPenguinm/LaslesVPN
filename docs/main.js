cd28478961221873efc46bde284e75b652776607
MIF <muhammadvvn0@gmail.com>
2025-07-02 17:16:39 +0300

    :)

--- docs/main.js
+++ docs/main.js
@@ -28,7 +28,7 @@ const testimonials = [
     rating: 4.5,
     comment:
       "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    image: "../assets/user3.png",
+    image: "./assets/user3.png",
   },
   {
     name: "Yessica Christy",
@@ -36,7 +36,7 @@ const testimonials = [
     rating: 4.5,
     comment:
       "I like it because I like to travel far and still can connect with high speed.",
-    image: "../assets/user1.png",
+    image: "./assets/user1.png",
   },
   {
     name: "Kim Young Jou",
@@ -44,7 +44,7 @@ const testimonials = [
     rating: 4.5,
     comment:
       "This is very unusual for my business that currently requires a virtual private network that has high security.",
-    image: "../assets/user2.png",
+    image: "./assets/user2.png",
   },
   {
     name: "Michael Chen",
@@ -52,7 +52,7 @@ const testimonials = [
     rating: 5.0,
     comment:
       "The best VPN service I've ever used. Perfect for remote work and accessing region-locked content. Highly recommended!",
-    image: "../assets/user3.png",
+    image: "./assets/user3.png",
   },
   {
     name: "Sarah Williams",
@@ -60,7 +60,7 @@ const testimonials = [
     rating: 4.8,
     comment:
       "Excellent service with great speeds. I use it daily for both work and streaming. The customer support is outstanding.",
-    image: "../assets/user2.png",
+    image: "./assets/user2.png",
   },
   {
     name: "Alex Rodriguez",
@@ -68,7 +68,7 @@ const testimonials = [
     rating: 4.7,
     comment:
       "Been using it for 6 months now. The connection is stable and secure. Perfect for my online banking and business needs.",
-    image: "../assets/user1.png",
+    image: "./assets/user1.png",
   },
   {
     name: "Emma Thompson",
@@ -76,7 +76,7 @@ const testimonials = [
     rating: 4.9,
     comment:
       "Amazing VPN service with fantastic global coverage. Never had any issues with speed or connectivity. Worth every penny!",
-    image: "../assets/user2.png",
+    image: "./assets/user2.png",
   },
 ];
 
@@ -102,7 +102,7 @@ function generateTestimonials() {
           </div>
           <div class="flex items-center gap-2">
             <span class="font-medium">${testimonial.rating}</span>
-            <img src="../assets/star.png" alt="star" class="w-4 h-4">
+            <img src="./assets/star.png" alt="star" class="w-4 h-4">
           </div>
         </div>
         <p class="text-gray-600">"${testimonial.comment}"</p>
